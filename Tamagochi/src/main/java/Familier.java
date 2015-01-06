da1a7faa1042e8ef37def4e5763cee1b995bb8fc
Naim KISSI <11223892@r10115.iutv.univ-paris13.fr>
2015-01-06 09:27:37 +0100

    Familier

---
+++ Tamagochi/src/main/java/Familier.java
@@ -0,0 +1,24 @@
+
+public class Familier {
+ private static int vieMax=30;
+ private int vie;
+ private String nom;
+ private ComportementMourrir comportement;
+ public Familier( String noms){
+	 this.nom=noms;
+ }
+ 
+ public void perdreVie(){
+	 
+ }
+ public void nourrir(){
+	 
+ }
+ 
+ public void executeMort(){
+	 
+ }
+ public void setComportement(ComportementMourrir comp){
+	 this.comportement=comp;
+ }
+}
